a5229f6834f2a0943800b5282212c60e9fdc754d
Víctor Tavares <victor.tavares.rafael@gmail.com>
2025-08-12 03:55:20 -0300

    refactor: Ajuste nos import's e na lógica do Drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,83 +1,26 @@
 import * as React from 'react';
 import { styled, useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery'; // Importação adicionada
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { useMediaQuery, Box, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import GroupsIcon from '@mui/icons-material/Groups';
 
 const drawerWidth = 240;
-
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
-    open?: boolean;
-}>(({ theme }) => ({
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: `-${drawerWidth}px`,
-    variants: [
-        {
-            props: ({ open }) => open,
-            style: {
-                transition: theme.transitions.create('margin', {
-                    easing: theme.transitions.easing.easeOut,
-                    duration: theme.transitions.duration.enteringScreen,
-                }),
-                marginLeft: 0,
-            },
-        },
-    ],
-}));
 
 const DrawerHeader = styled('div')(({ theme }) => ({
     display: 'flex',
     alignItems: 'center',
     padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
     ...theme.mixins.toolbar,
     justifyContent: 'flex-end',
 }));
 
 export default function PersistentDrawerLeft() {
     const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down('sm')); // Verifica se é mobile
-    const [open, setOpen] = React.useState(!isMobile); // Define o estado inicial com base no tamanho da tela
-
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+    const [open, setOpen] = React.useState(!isMobile);
 
     return (
         <Box sx={{ display: 'flex' }}>
-            <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={handleDrawerOpen}
-                edge="start"
-                sx={[
-                    {
-                        mr: 2,
-                    },
-                    open && { display: 'none' },
-                ]}
-            >
-                <MenuIcon />
-            </IconButton>
             <Drawer
                 sx={{
                     width: drawerWidth,
@@ -93,9 +36,6 @@ export default function PersistentDrawerLeft() {
             >
                 <DrawerHeader sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
                     <img src="src/assets/images/logo.png" width="100px" alt="Logo" />
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                    </IconButton>
                 </DrawerHeader>
                 <List>
                     <ListItem key={"Colaboradores"} disablePadding sx={{ display: 'block' }}>
@@ -108,9 +48,6 @@ export default function PersistentDrawerLeft() {
                     </ListItem>
                 </List>
             </Drawer>
-            <Main open={open}>
-                <DrawerHeader />
-            </Main>
         </Box>
     );
 }
